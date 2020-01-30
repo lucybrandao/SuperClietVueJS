a085275f98c040337562cd125a5491d6549a6644
lucybrandao <lucyjbrandao@gmail.com>
2020-01-30 10:32:29 -0300

    github pages config

---
+++ precache-manifest.df0ef18611af9bd1a4ea9932054dd9ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9403c98a38db0bf7664e",
+    "url": "/css/chunk-vendors.4cf8287f.css"
+  },
+  {
+    "revision": "41d7ec75657becb09f1088dd168a5cec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2de4349df5da8a3e7324",
+    "url": "/js/app.25ac6c36.js"
+  },
+  {
+    "revision": "9403c98a38db0bf7664e",
+    "url": "/js/chunk-vendors.159ab69e.js"
+  },
+  {
+    "revision": "fb1a7f1d9395cae26d837f8226eb0b91",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
